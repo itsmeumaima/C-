3678bcd510a62e4658d4f77a040d7c1a3b1a526d
Umaima <umaimarauf2005@gmail.com>
2024-02-13 20:01:18 +0500

    First program added

---
+++ program1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=12;
+    int sum=a+b;
+    int sub=b-a;
+    cout<<"The addition of "<<a<<" and "<<b<<" is:"<<sum;
+    cout<<"\nThe subtraction of "<<a<<" and "<<b<<" is:"<<sub;
+}
